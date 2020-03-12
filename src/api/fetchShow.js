6caffae4370a589008c018b6cb8ae152b3c3c6a5
Blair <abrgroup@gmail.com>
2020-03-11 22:04:03 -0400

    test working in app.test.js

---
+++ src/api/fetchShow.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+
+export const fetchShow = () => {
+  return axios
+    .get(
+      "https://api.tvmaze.com/singlesearch/shows?q=stranger-things&embed=episodes"
+    )
+    .then(res => {
+      console.log(res);
+      return res;
+    })
+    .catch(err => {
+      console.error("error fetching data from api, err: ", err.message);
+      return err;
+    });
+
+  //     setShow(res.data);
+  //     setSeasons(formatSeasons(res.data._embedded.episodes));
+  //   });
+};
